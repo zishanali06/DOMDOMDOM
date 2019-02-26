0911dd9bedeb769a538e9535aaec813779c5913a
Zishan Ali <zishanali06@gmail.com>
2019-02-25 21:24:49 -0600

    finished DOM DOM DOM

--- scripts.js
+++ scripts.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', function(){
+document.addEventListener('DOMContentLoaded', function () {
     function getRndInt(min, max) {
         return Math.floor(Math.random() * (max - min)) + min;
     };
@@ -14,7 +14,7 @@ document.addEventListener('DOMContentLoaded', function(){
     document.body.appendChild(newrowdiv);
 
     let squareid = 1;
-    button1.addEventListener("click", function(){
+    button1.addEventListener("click", function () {
         let newDiv = document.createElement('div');
         newDiv.className = 'boxes';
         newDiv.id = squareid;
@@ -22,12 +22,11 @@ document.addEventListener('DOMContentLoaded', function(){
 
         document.body.appendChild(newDiv);
 
-        newDiv.addEventListener('mouseenter', function(){
+        newDiv.addEventListener('mouseenter', function () {
             let squaretext = document.createTextNode(newDiv.id);
             newDiv.appendChild(squaretext);
 
-            newDiv.addEventListener('mouseleave', function(){
-                console.log('need to be in here');
+            newDiv.addEventListener('mouseleave', function () {
                 let removesquaretext = document.createTextNode("");
 
                 newDiv.replaceChild(removesquaretext, squaretext);
@@ -35,8 +34,34 @@ document.addEventListener('DOMContentLoaded', function(){
         });
 
         let colors = ['red', 'blue', 'green', 'purple'];
-        newDiv.addEventListener('click', function(){
-            newDiv.style.backgroundColor = colors[getRndInt(0,5)];
+        newDiv.addEventListener('click', function () {
+            newDiv.style.backgroundColor = colors[getRndInt(0, 5)];
         });
+        newDiv.addEventListener('dblclick', function () {
+            let clicksquareid = newDiv.id;
+
+            if (clicksquareid % 2 == 0) {
+                clicksquareid++;
+                let newsquareselect = document.getElementById(clicksquareid);
+                if(newsquareselect == null){
+                    alert('There is no Box to Remove After');
+                }else{
+                    newsquareselect.remove();
+                };
+            }else{
+                clicksquareid--;
+                let newsquareselect = document.getElementById(clicksquareid);
+                if(newsquareselect == null){
+                    alert('There is no box to Remove Before');
+                }else{
+                    newsquareselect.remove();
+                }
+            };
+        })
     });
 });
+
+
+//question 1, my console keeps getting an error.
+
+//questoin 2, i cheated and used table-cell as display attribute but I what other way can I get the boxes to not move, when i was using display: flex or even display: inline-block the div square would move when the number text went into it.
